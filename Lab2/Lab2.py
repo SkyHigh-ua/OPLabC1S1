f69091c334c92c500705c147b795e309664c12fa
SkyHighConstruction <80775870+SkyHighConstruction@users.noreply.github.com>
2021-09-23 11:20:03 +0300

    Delete Lab2 directory

--- Lab2/Lab2.py
+++
@@ -1,6 +0,0 @@
-from math import fabs
-x, y = map(float, input( 'Введіть координати точки: ' ).split())
-if x >= -1 and y <= 1 and y >= fabs(x):
-    print("Точка належить")
-else:
-    print("Точка не належить")
