dba03308a5b518061b8e4a681b4b73f0b49bb4ea
SkyHighConstruction <80775870+SkyHighConstruction@users.noreply.github.com>
2021-10-03 14:22:10 +0300

    Update Lab3.py

--- Lab3/Lab3.py
+++ Lab3/Lab3.py
@@ -3,9 +3,11 @@ acur = int( input( "Введіть точність ε = " ) )
 n = int( input( "Введіть точність обчислень = " ) )
 sumfx = 0
 h = pi / n
-for i in range(1, n+1):
+i = 1
+while i <= n:
     x = i * h - h / 2
     fx = log( 2 + sin(x) )
     sumfx += fx
+    i += 1
 result = h * sumfx
 print( "Результат =", round( result, acur ) )
