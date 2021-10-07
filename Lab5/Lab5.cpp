cb6afb2d7b10f6c1cbd9bce0d171925f5faed6f0
SkyHighConstruction <80775870+SkyHighConstruction@users.noreply.github.com>
2021-10-07 11:05:43 +0300

    Update Lab5.cpp

--- Lab5/Lab5.cpp
+++ Lab5/Lab5.cpp
@@ -1,15 +1,14 @@
 #include <iostream>
-#include <string> 
 
 int main()
 {
     int i , i1, i2, i3, i4;
     for (i = 1000; i <= 9999; i++)
     {
-        i1 = i mod 10
-        i2 = i mod 100 div 10
-        i3 = i div 100 mod 10
-        i4 = i div 1000
+        i1 = i % 10;
+        i2 = i % 100 / 10;
+        i3 = i / 100 % 10;
+        i4 = i / 1000;
         if (i1==i4 && i2==i3)
         {
             std::cout << i << '\n';
