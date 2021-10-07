5e3463f035e74d17915eef85bac7f2981417ec2b
SkyHighConstruction <80775870+SkyHighConstruction@users.noreply.github.com>
2021-10-07 10:58:40 +0300

    Update Lab5.py

--- Lab5/Lab5.py
+++ Lab5/Lab5.py
@@ -1,6 +1,9 @@
 for i in range (1000, 10000):
-    i = str(i)
-    if i[0]==i[3] and i[1]==i[2]:
+    i1 = i % 10
+    i2 = i % 100 // 10
+    i3 = i // 100 % 10
+    i4 = i // 1000
+    if i1==i4 and i2==i3:
         print(i)
     else:
         continue
